525a90113ac5c5ed1bf6c252388fc0cc9db201bc
H4drian <leoncotten6@gmail.com>
2024-07-09 17:27:51 -0400

    Updated main.c

--- src/main.c
+++ src/main.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define ANSI_PINK "\033[95m"
+#define ANSI_BOLD "\033[1m"
+#define ANSI_RESET "\033[0m"
+#define RESET_TO_PINK ANSI_RESET ANSI_PINK
 
 int main() {
-  puts("╭────────FREAKY────────╮");
-  puts(" OS: Freakbuntu         ");
-  puts(" Host: FreakyServer     ");
-  puts(" Kernel: Freaknix       ");
-  puts(" Shell: Freaksh         ");
-  puts(" WM: Freakyland         ");
-  puts(" Terminal: Alacfreaky   ");
-  puts(" Font: Hack Freaky Font ");
-  puts("╰──────────────────────╯");
+  system("chafa freaky.png");
+  puts(ANSI_PINK"╭──────────👅" ANSI_BOLD "FREAKY" ANSI_RESET ANSI_PINK"👅──────────╮  ");
+  puts(" " ANSI_BOLD "OS"       RESET_TO_PINK":       👅Freakbuntu👅         ");
+  puts(" " ANSI_BOLD "Host"     RESET_TO_PINK":     👅FreakyServer👅         ");
+  puts(" " ANSI_BOLD "Kernel"   RESET_TO_PINK":   👅Freaknix👅               ");
+  puts(" " ANSI_BOLD "Shell"    RESET_TO_PINK":    👅Freaksh👅               ");
+  puts(" " ANSI_BOLD "WM"       RESET_TO_PINK":       👅Freakyland👅         ");
+  puts(" " ANSI_BOLD "Terminal" RESET_TO_PINK": 👅Alacfreaky👅               ");
+  puts(" " ANSI_BOLD "Font"     RESET_TO_PINK":     👅Hack Freaky Font👅     ");
+  puts("╰──────────────────────────────╯  ");
 
   return 0;
 }
