2768145a983c884626d3b9ed18f0d929bf7ccfa7
H4drian <leoncotten6@gmail.com>
2024-07-09 17:03:10 -0400

    Updated main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main() {
-  
+  puts("╭────────FREAKY────────╮");
+  puts(" OS: Freakbuntu         ");
+  puts(" Host: FreakyServer     ");
+  puts(" Kernel: Freaknix       ");
+  puts(" Shell: Freaksh         ");
+  puts(" WM: Freakyland         ");
+  puts(" Terminal: Alacfreaky   ");
+  puts(" Font: Hack Freaky Font ");
+  puts("╰──────────────────────╯");
+
+  return 0;
 }
